46908fbc74b357b590f3f32b53efb577fdcbc62d
Jack Cota <jack@cota.co.uk>
2022-09-25 23:07:48 +0100

    Update register page to use database

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, redirect, url_for
 from users.forms import RegisterForm, LoginForm
+from app import db
+from models import User
 
 users_blueprint = Blueprint('users', __name__, template_folder='templates')
 
@@ -9,8 +11,17 @@ def register():
     form = RegisterForm()
 
     if form.validate_on_submit():
-        print(form.username.data)
-        print(form.password.data)
+        user = User.query.filter_by(username=form.username.data).first()
+
+        if user:
+            flash("Username already exists")
+            return render_template('register.html', form=form)
+
+        new_user = User(username=form.username.data, password=form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('users.login'))
 
     return render_template('register.html', form=form)
 
