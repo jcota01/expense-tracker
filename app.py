19beb542f435696cabfba8a0fca78854fcef8e13
Jack Cota <jack@cota.co.uk>
2022-09-25 23:07:18 +0100

    Add db configs to main app

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import os
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expense-tracker.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-
-@app.route('/')
-def index():
-    return render_template("index.html", title="Home", description="hello")
+db = SQLAlchemy(app)
 
 
 if __name__ == '__main__':
@@ -16,7 +16,9 @@ if __name__ == '__main__':
 
     # BLUEPRINTS
     from users.views import users_blueprint
+    from expenses.views import expenses_blueprint
 
     app.register_blueprint(users_blueprint)
+    app.register_blueprint(expenses_blueprint)
 
     app.run(host=my_host, debug=True)
